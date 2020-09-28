1005a8cd6f449e3b5c55ef9b6271953ae63e451c
Oxxy-k <70562469+Oxxy-k@users.noreply.github.com>
2020-09-28 12:58:49 +0300

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,67 +1,139 @@
-const render = ({ todos, editTodoId }) =>
+const getFilterActive = (toDoList) => toDoList.filter(({ status }) => !status);
+
+const getFilterCompleted = (toDoList) =>
+  toDoList.filter(({ status }) => status);
+
+const generateId = (() => {
+  let count = 0;
+  return () => ++count;
+})();
+const filterTodos = (todos, filter) =>
+  filter === "all"
+    ? todos
+    : filter === "active"
+    ? getFilterActive(todos)
+    : filter === "completed"
+    ? getFilterCompleted(todos)
+    : todos;
+
+const onSetFilter = (filter) => {
+  setState({ filter });
+};
+
+const render = ({ todos, editTodoId, filter }) =>
   `
-         ${todos
+         ${filterTodos(todos, filter)
            .map((todo) =>
              todo.id === editTodoId
                ? `
+        
         <tr>
-          <td class="do-list" value='${todo.title}'></td>
-          <td><button class="delete">&#10008</button></td>
-          <td><button class="complete">&#10004</button></td>
-        </tr>`
+          <td><button class="delete" id='delete' onclick='onRemoveTodo(${todo.id})'>&#10008</button></td>
+          <td><button class="complete" id='complete' onclick='onChangeStatusTodo(${todo.id})'>&#10004</button></td>
+          <td name='title' type='text' class="do-list">${todo.title}</td>
+        </tr>
+        `
                : `
-        <tr >
-          <td class="do-list ${todo.status ? "complete-do" : ""}">${
-                   todo.title
-                 }</td>
-          <td><button class="delete">&#10008</button></td>
-          <td><button class="complete">&#10004</button></td>
-        </tr>`
+        <tr >          
+          <td><button class="delete" id='delete' onclick='onRemoveTodo(${
+            todo.id
+          })'>&#10008</button></td>
+          <td><button class="complete" id='complete' onclick='onChangeStatusTodo(${
+            todo.id
+          })')'>&#10004</button></td>
+          <td class="do-list ${todo.status ? ` complete-do"` : `"`}
+           >${todo.title}</td>
+          </tr>`
            )
            .join(" ")}
-      
-    </div>`;
+                 
+    `;
 
-const state = {
+let state = {
   todos: [
-    { id: 1, title: "My title", status: false },
-    { id: 2, title: "Foo", status: true },
-    { id: 3, title: "Bar", status: false },
+    { id: generateId(), title: "title", status: false },
+    { id: generateId(), title: "Complete", status: true },
+    { id: generateId(), title: "Bar", status: false },
+    { id: generateId(), title: "Complete", status: true },
+    { id: generateId(), title: "Edit", status: false },
+    { id: generateId(), title: "complete", status: true },
   ],
-  editTodoId: 3,
+  editTodoId: 0,
+  filter: "all",
 };
 
+const renderToDom = (template) => {
+  document.getElementById("doTable").innerHTML = template;
+};
+const setState = (newStatePart) => {
+  state = { ...state, ...newStatePart };
+  const upgrade = render(state);
+  renderToDom(upgrade);
+};
+const clearCompleted = () => {
+  setState({ todos: getFilterActive(state.todos) });
+};
+
+const clearDuring = () => {
+  setState({ todos: getFilterCompleted(state.todos) });
+};
+
+const onEditToDo = (editTodoId) => {
+  setState({
+    editTodoId,
+  });
+};
 const main = () => {
   document.getElementById("doTable").innerHTML = render(state);
 };
+const filterAll = () => {
+  document.getElementById("doTable").innerHTML = render(stateBegin);
+};
+main();
 
-const generateId = (() => {
-  let _id = 0;
-  return () => ++_id;
-})();
+const addToDo = (toDoList, { title, status = false }) => [
+  ...toDoList,
+  { id: generateId(), title: title, status: status },
+];
 
-function addToDo(toDoList, name) {
-  if (!name) return;
-  let newToDo = {
-    id: generateId(),
-    status: false,
-    name,
+const deleteToDo = (toDoList, toDoId) =>
+  toDoList.filter(({ id }) => id !== toDoId);
+
+function changeStatusToDo(toDoList, toDoId) {
+  const Id = toDoList.findIndex((toDoList) => toDoList.id == toDoId);
+  toDoList[Id].status = toDoList[Id].status ? false : true;
+  return [...toDoList];
+}
+
+const changeNameToDo = (toDoList, toDoId, name) =>
+  toDoList.map((toDo) => (toDo.id === toDoId ? { ...toDo, name } : toDo));
+
+const onNewAdd = (formElement, event) => {
+  event.preventDefault();
+  const formData = getForm(formElement);
+  const newData = {
+    title: formData.title,
+    status: formData.status === "on",
   };
-  return [...toDoList, newToDo];
-}
+  setState({ todos: addToDo(state.todos, newData) });
+};
+const getForm = (formElement) => {
+  const formData = new FormData(formElement);
+  const data = {};
+  for (let [key, values] of formData.entries()) {
+    data[key] = values;
+  }
+  return data;
+};
 
-function deleteToDo(toDoList, toDoId) {
-  return toDoList.filter(({ id }) => id !== toDoId);
-}
+const onRemoveTodo = (todoId) => {
+  setState({
+    todos: deleteToDo(state.todos, todoId),
+  });
+};
 
-function changeStatusToDo(toDoList, toDoId, status) {
-  return toDoList.map((toDo) =>
-    toDo.id === toDoId ? { ...toDo, status } : toDo
-  );
-}
-
-function changeNameToDo(toDoList, name) {
-  return toDoList.map((toDo) =>
-    toDo.id === toDoId ? { ...toDo, name } : toDo
-  );
-}
+const onChangeStatusTodo = (todoId) => {
+  setState({
+    todos: changeStatusToDo(state.todos, todoId),
+  });
+};
